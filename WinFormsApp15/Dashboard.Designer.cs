d93235424c4e14576aa00ca0a05a5e812554e36f
Jaycii <justineclaire0409@gmail.com>
2026-03-02 11:13:52 +0800

    Add files via upload

---
+++ WinFormsApp15/Dashboard.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinFormsApp15
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnregister_student = new Button();
+            btnstudent_list = new Button();
+            SuspendLayout();
+            // 
+            // btnregister_student
+            // 
+            btnregister_student.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnregister_student.Location = new Point(65, 178);
+            btnregister_student.Name = "btnregister_student";
+            btnregister_student.Size = new Size(260, 64);
+            btnregister_student.TabIndex = 0;
+            btnregister_student.Text = "REGISTER STUDENT";
+            btnregister_student.UseVisualStyleBackColor = true;
+            btnregister_student.Click += button1_Click;
+            // 
+            // btnstudent_list
+            // 
+            btnstudent_list.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnstudent_list.Location = new Point(361, 178);
+            btnstudent_list.Name = "btnstudent_list";
+            btnstudent_list.Size = new Size(260, 64);
+            btnstudent_list.TabIndex = 1;
+            btnstudent_list.Text = "STUDENT LIST";
+            btnstudent_list.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(705, 450);
+            Controls.Add(btnstudent_list);
+            Controls.Add(btnregister_student);
+            Name = "Dashboard";
+            Text = "Dashboard";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnregister_student;
+        private Button btnstudent_list;
+    }
+}
---
+++ WinFormsApp15/Login.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WinFormsApp15
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblusername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnlogin = new Button();
+            SuspendLayout();
+            // 
+            // lblusername
+            // 
+            lblusername.AutoSize = true;
+            lblusername.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblusername.Location = new Point(21, 41);
+            lblusername.Name = "lblusername";
+            lblusername.Size = new Size(107, 24);
+            lblusername.TabIndex = 0;
+            lblusername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(21, 84);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(103, 24);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtUsername.Location = new Point(134, 33);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(304, 32);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(134, 76);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(304, 32);
+            txtPassword.TabIndex = 3;
+            txtPassword.UseSystemPasswordChar = true;
+            txtPassword.TextChanged += txtPassword_TextChanged;
+            // 
+            // btnlogin
+            // 
+            btnlogin.BackColor = SystemColors.ActiveBorder;
+            btnlogin.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnlogin.ForeColor = Color.Black;
+            btnlogin.Location = new Point(98, 142);
+            btnlogin.Name = "btnlogin";
+            btnlogin.Size = new Size(275, 51);
+            btnlogin.TabIndex = 13;
+            btnlogin.Text = "LOGIN";
+            btnlogin.UseVisualStyleBackColor = false;
+            btnlogin.Click += btnlogin_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(450, 246);
+            Controls.Add(btnlogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblusername);
+            Name = "Login";
+            Text = "Login";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblusername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnlogin;
+    }
+}
